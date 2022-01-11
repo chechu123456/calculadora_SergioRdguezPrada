6b0ec93f37068ed2c2bf0802158f2148632d0b6e
Sergio Rodríguez Prada <chechu_sergio@live.com>
2022-01-11 20:30:20 +0100

    Calculadora en PHP y Java Documentado

---
+++ CalculadoraPhp/html/search/variables_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_24num2_0',['$num2',['../proceso_8php.html#a02d5187f58e5a2f321037d31629ba119',1,'proceso.php']]],
+  ['_24operacion_1',['$operacion',['../proceso_8php.html#a414f2abb5c4590f83940130b9919cfba',1,'proceso.php']]]
+];
---
+++ CalculadoraPhp/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['else_0',['else',['../proceso_8php.html#a8c3e50a643dc89d77f0a6ed9ae6c1596',1,'proceso.php']]]
+];
