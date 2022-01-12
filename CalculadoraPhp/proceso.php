314888d3dd58c1c1d8ea5317fab7e3f1f297d377
Sergio Rodríguez Prada <chechu_sergio@live.com>
2022-01-12 20:48:51 +0100

    Documentación de PHP actualizada

--- CalculadoraPhp/proceso.php
+++ CalculadoraPhp/proceso.php
@@ -8,12 +8,18 @@
 </head>
 <body>
     <?php
+        /**
+         * 
+         * @author Sergio Rodríguez Prada
+         * 
+         */
+
         if(!isset($_POST['num1']) || !isset($_POST['num2']) || !isset($_POST['operacion'])){
             echo "<p>Error, Algún caja está vacía</p>";
             echo "<p><a href='calculadora.html'>Volver al inicio </a></p>";
         }else{
-            $num1 = (int)$_POST['num1'];
-            $num2 = (int)$_POST['num2'];
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
             $operacion = $_POST['operacion'];
 
             if($num1 = 0){
@@ -27,32 +33,77 @@
                 switch($operacion){
                     case "sumar":
                         echo "<h3>El resultado es: </h3>";
-                        echo "<p>". $num1+$num2 . "</p>";
+                        sumar($num1,$num2);
                         break;
 
                     case "restar":
                         echo "<h3>El resultado es: </h3>";
-                        echo "<p>". $num1-$num2 . "</p>";
+                        restar($num1,$num2);
                         break;
 
                     case "multiplicar":
                         echo "<h3>El resultado es: </h3>";
-                        echo "<p>". $num1*$num2 . "</p>";
+                        multiplicar($num1,$num2);
                         break;
 
                     case "dividir":
-                        if($num2 == 0){
-                            echo "<p>ERROR: No se puede dividir entre 0 </p>";
-                            echo "<p><a href='calculadora.html'>Volver al inicio </a></p>";
-                        }else{
-                            echo "<h3>El resultado es: </h3>";
-                            echo  "<p>". $num1/$num2 ;
-                        }
+                        echo "<h3>El resultado es: </h3>";
+                        dividir($num1,$num2);
                         break;
                     default:
                         echo "ERROR: operación no válida";
                         echo "<p><a href='calculadora.html'>Volver al inicio </a></p>";
                 }
+            }
+        }
+
+        /**
+         * 
+         * @return int la suma de 2 valores
+         * @param int $num1 numero1
+         * @param int $num2 numero2
+         * 
+         */
+        function sumar($num1,$num2){
+           return $num1+$num2;
+        }
+
+        /**
+         * 
+         * @return int la resta de 2 valores
+         * @param int $num1 numero1
+         * @param int $num2 numero2
+         * 
+         */
+        function restar($num1,$num2){
+            return $num1-$num2;
+        }
+
+        /**
+         * 
+         * @return int la multiplicación de 2 valores
+         * @param int $num1 numero1
+         * @param int $num2 numero2
+         * 
+         */
+        function multiplicar($num1,$num2){
+            return $num1*$num2;
+        }
+
+        /**
+         * 
+         * @return double la división de 2 valores
+         * @param int $num1 numero1
+         * @param int $num2 numero2
+         * 
+         */
+        function dividir($num1,$num2){
+            if($num2 == 0){
+                $html = "<p>ERROR: No se puede dividir entre 0 </p>" .
+                 "<p><a href='calculadora.html'>Volver al inicio </a></p>";
+                return $html;
+            }else{
+                return $num1/$num2 ;
             }
         }
     ?>
